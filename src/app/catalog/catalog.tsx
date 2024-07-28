39051b23eadf3f68a1fa88593c36566e4da4eafa
DatsunCo <danya200375d@gmail.com>
2024-07-28 22:30:51 +0300

    init: eslit

---
+++ src/app/catalog/catalog.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const catalog = () => {
+  return <div></div>;
+};
+
+export default catalog;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
+import React from "react";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/utils"
- 
+import { cn } from "@/lib/utils";
+
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,10 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
+      <body
+        className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}>{children}</body>
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
